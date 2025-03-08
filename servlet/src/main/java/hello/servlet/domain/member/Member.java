f2c91f37fb4b3f4b781f05092b42c87e4159e0da
seo hyun seok <tjgustjr33@naver.com>
2025-03-08 17:35:25 +0900

    suction4

---
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,19 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.age = age;
+        this.username = username;
+    }
+
+    public Member() {
+    }
+}
---
+++ servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member savedMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+        //Assertions.assertThat.. 생성후 Assertions에 alt + enter 하고 static 란 엔터시
+        // assertThat(members).isEqualTo(2);로  깔끔하게 바뀜
+    }
+}
