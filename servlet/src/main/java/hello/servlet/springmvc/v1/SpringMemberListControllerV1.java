2a8921b6577149cfe1872b1ab37aa960681c88de
seo hyun seok <tjgustjr33@naver.com>
2025-03-13 00:53:54 +0900

    Complete spring MVC - structure Step 6

---
+++ servlet/src/main/java/hello/servlet/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,19 @@
+package hello.servlet.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+public class SpringMemberListControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
