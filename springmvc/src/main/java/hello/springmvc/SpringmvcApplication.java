e38db4b8123ae133cbe29a9a8f0350fe5a9ad824
seohyunseok <tjgustjr33@gmail.com>
2025-03-29 02:38:59 +0900

    checking

--- springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
+++ springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication {
-
+	//s
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
